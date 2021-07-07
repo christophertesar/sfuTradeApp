8b6e503cff25aa2f966af3166a6df9837028031e
christophertesar <ctesar@sfu.ca>
2021-07-07 15:11:24 -0700

    testing with chris firebase implementation

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,20 +1,61 @@
 package com.example.myfirstapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Context mContext = MainActivity.this;
+
+    private FirebaseAuth mAuth;
+
+    /**
+     * Setup of Firebase authenticator
+     */
+    private void setupFirebaseAuth(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void checkCurrentUser(FirebaseUser user){
+        if(user == null){
+            Intent intent = new Intent(mContext, MainActivity.class);
+            startActivity(intent);
+
+        }
+    }
+
+    /**
+     --------------------------------------------------------------------------------------------
+     **/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+
+        setupFirebaseAuth();
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -42,5 +83,4 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
-
 }

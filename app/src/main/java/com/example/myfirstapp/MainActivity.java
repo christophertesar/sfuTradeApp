949403b0a60f979ce12b05702326c78d24190d7d
christophertesar <ctesar@sfu.ca>
2021-07-10 13:02:03 -0700

    -Implemented Firebase authentication -Link buttons to sign in -Signout test

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,33 +1,72 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+
 import android.content.Context;
 import android.content.Intent;
-import android.nfc.Tag;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext = MainActivity.this;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //UI
+    private EditText mEmail, mPassword;
+    private Button btnSignIn, btnSignOut;
 
     /**
      * Setup of Firebase authenticator
      */
     private void setupFirebaseAuth(){
         mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null) {
+                    toastMessage("Successfully signed in with:" + user.getEmail());
+                }
+                else {
+                    toastMessage("Successfully signed out.");
+                }
+            }
+        };
     }
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
         FirebaseUser currentUser = mAuth.getCurrentUser();
         //updateUI(currentUser);
+    }
+
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 
     private void checkCurrentUser(FirebaseUser user){
@@ -47,7 +86,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        mPassword = (EditText) findViewById(R.id.editTextTextPassword);
+        btnSignIn = (Button) findViewById(R.id.button);
+        btnSignOut = (Button) findViewById(R.id.button2);
+
         setupFirebaseAuth();
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                if(!email.equals("") && !pass.equals("")){
+                    mAuth.signInWithEmailAndPassword(email,pass);
+                }
+                else{
+                    toastMessage("Fields not filled.");
+                }
+
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mAuth.signOut();
+                toastMessage("Signing out.");
+            }
+        });
 
     }
 }

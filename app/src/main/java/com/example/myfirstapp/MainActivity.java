177799c4adbac64d6c693de9854298a69dfa0f88
Alisa <jekoh@sfu.ca>
2021-07-09 18:05:47 -0700

    Linked the login button to the main screen app. - removed the firebase dependencies(in build.gradle & MainActivity) because it was failing gradle sync

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,46 +5,16 @@ import android.content.Intent;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.view.MenuItem;
+
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Context mContext = MainActivity.this;
-
-    private FirebaseAuth mAuth;
 
     /**
-     * Setup of Firebase authenticator
-     */
-    private void setupFirebaseAuth(){
-        mAuth = FirebaseAuth.getInstance();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
-    }
-
-    private void checkCurrentUser(FirebaseUser user){
-        if(user == null){
-            Intent intent = new Intent(mContext, MainActivity.class);
-            startActivity(intent);
-
-        }
-    }
-
-    /**
-     --------------------------------------------------------------------------------------------
+     * --------------------------------------------------------------------------------------------
      **/
 
     @Override
@@ -52,35 +22,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupFirebaseAuth();
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
+//        setupFirebaseAuth();
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainApp.class);
+                startActivity(intent);
+            }
+        });
 
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment selectedFragment = null;
-
-                    switch (menuItem.getItemId()) {
-                        case R.id.nav_dashboard:
-                            selectedFragment = new DashboardFragment();
-                            break;
-
-                        case R.id.nav_post:
-                            selectedFragment = new PostFragment();
-                            break;
-
-                        case R.id.nav_market:
-                            selectedFragment = new MarketFragment();
-                            break;
-                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-
-                    return true;
-                }
-            };
 }
+
+

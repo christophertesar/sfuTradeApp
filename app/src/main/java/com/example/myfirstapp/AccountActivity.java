fc41f549208d9ac862746b67c1cf5d60bc96c443
Alisa <jekoh@sfu.ca>
2021-07-21 15:17:05 -0700

    Registering users will now be added into the database.

--- app/src/main/java/com/example/myfirstapp/AccountActivity.java
+++ app/src/main/java/com/example/myfirstapp/AccountActivity.java
@@ -1,11 +1,34 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+import org.w3c.dom.Text;
+
 public class AccountActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +39,41 @@ public class AccountActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button logout = (Button) findViewById(R.id.btn_signout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(AccountActivity.this, MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(AccountActivity.this,"You have succesfully sign out",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+
+//        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                User userProfile = snapshot.getValue(User.class);
+//
+//                if (userProfile != null){
+////                    String email = userProfile.email;
+//                    String email = user.getEmail();
+//                    emailTextView.setText(email);
+//                }
+//            }
+
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                Toast.makeText(AccountActivity.this, "Something wrong happened!", Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 }
